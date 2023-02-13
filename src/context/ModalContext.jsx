30779d921290183ca517a928be135479dfe456fc
EhsonjonKomiljonov <ekomiljonov29@gmail.com>
2023-02-13 14:09:54 +0500

    close post product modal

--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const ModalContext = createContext();
 
 export const ModalProvider = ({ children }) => {
-  const [modal, setModal] = useState(true);
+  const [modal, setModal] = useState(false);
 
   return (
     <ModalContext.Provider value={{ modal, setModal }}>
